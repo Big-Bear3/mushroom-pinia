fb8175b7d79c408489912ab577e722717630ae13
BigBear3 <yyizp@vip.qq.com>
2024-03-18 11:54:25 +0800

    Limited test coverage file

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,8 +8,10 @@ export default mergeConfig(
         test: {
             environment: 'jsdom',
             exclude: [...configDefaults.exclude, 'e2e/*'],
-            root: fileURLToPath(new URL('./', import.meta.url))
+            root: fileURLToPath(new URL('./', import.meta.url)),
+            coverage: {
+                include: ['src/mushroom-pinia-core/src/*', 'src/stores/*']
+            }
         }
     })
 );
-
